c46962c3ac26d6f63d28cd3811c76ef8133f6b88
Carol Capek <carol.capek@mohemian.com>
2016-11-14 11:13:49 +0100

    Add formular

---
+++ lokalise.rb
@@ -0,0 +1,15 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Lokalise < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/carol-mohemian/lokalise/archive/v1.0.0.tar.gz"
+  version "1.0.0"
+  sha256 "7e5fbc4cd1c90e44682c279e58f5ce1a1177b1c8d02a936ac07ae6eb3ca8b206"
+
+  def install
+      bin.install "lokalise"
+  end
+end
