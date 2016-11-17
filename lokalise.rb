b588f93d4702e3e6c060b01487fec557d877620d
Carol Capek <carol.capek@mohemian.com>
2016-11-17 10:35:53 +0100

    Change download url

--- lokalise.rb
+++ lokalise.rb
@@ -5,7 +5,8 @@
 class Lokalise < Formula
   desc ""
   homepage ""
-  url "https://github.com/carol-mohemian/lokalise/archive/v1.0.0.tar.gz"
+  url "https://lokalise.co/cli/lokalise-0.2b-darwin-amd64.tgz"
+  #url "https://github.com/carol-mohemian/lokalise/archive/v1.0.0.tar.gz"
   version "1.0.0"
   sha256 "7e5fbc4cd1c90e44682c279e58f5ce1a1177b1c8d02a936ac07ae6eb3ca8b206"
 
